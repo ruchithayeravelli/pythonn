fd0e75740a6200d2529d2863525516432d27eae2
Ruchitha <yeravelliruchitha@gmail.com>
2025-07-29 23:14:43 +0530

    Add files via upload

---
+++ typeint to float.py
@@ -0,0 +1,57 @@
+# inttt
+# n=55
+# print(float(n))    # ----int to float-----YES
+# print(complex(n))  #----int to  complex------YES
+# print(bool(n))     #-----int to bool ------YES
+# print(str(n))      #------- int to str-------YES
+# print(list(n))     #-----int to list-------NO
+# print(tuple(n))    #-----int to tuple--------NO
+# #float
+# f =55.0
+# print(int(f))      #------ float to int ---YES
+# print(float(f))    # ----f to float-----YES
+# print(complex(f))  #----f to  complex------YES
+# print(bool(f))     #-----f to bool ------YES
+# print(str(f))      #-------f to str-------YES
+# print(list(f))     #-----f to list-------NO
+# print(tuple(f))    #-----f to tuple--------NO
+# c=3+6j
+# print(int(c))      #------Complex to int ---NO
+# print(float(c))    # ----c to float-----NO
+# print(complex(c))  #----c to  complex------YES
+# print(bool(c))     #-----c to bool ------YES
+# print(str(c))      #-------c to str-------YES
+# print(list(c))     #-----c to list-------NO
+# print(tuple(c))    #-----c to tuple--------NO
+# b=True
+# print(int(b))      #------Boolen to int ---YES
+# print(float(b))    # ----b to float-----YES
+# print(complex(b))  #----b to  complex------YES
+# print(bool(b))     #-----b to bool ------YES
+# print(str(b))      #-------b to str-------YES
+# print(list(b))     #-----b to list-------NO
+# print(tuple(b))    #-----b to tuple--------NO
+# l=[2,4,5,1,7,1]
+# print(int(l))      #------list to int ---NO
+# print(float(l))    # ----l to float-----NO
+# print(complex(l))  #----l to  complex------NO
+# print(bool(l))     #-----l to bool ------YES
+# print(str(l))      #-------l to str-------YES
+# print(list(l))     #-----l to list-------YES
+# print(tuple(l))    #-----l to tuple--------YES
+# tu=(1,2,3,1,5,6,2)
+# print(int(tu))      #------Tuple to int ---NO
+# print(float(tu))    # ----tu to float-----NO
+# print(complex(tu))  #----tu to  complex------NO
+# print(bool(tu))     #-----tu to bool ------YES
+# print(str(tu))      #-------tu to str-------YES
+# print(list(tu))     #-----tu to list-------YES
+# print(tuple(tu))    #-----tu to tuple--------YES
+s="rtyuio"
+# print(int(s))      #------String to int ---YES
+# print(float(s))    # ---s to float-----YES
+# print(complex(s))  #----s to  complex------YES
+print(bool(s))     #-----s to bool ------YES
+print(str(s))      #-------s to str-------YES
+print(list(s))     #-----s to list-------NO
+print(tuple(s))    #-----s to tuple--------NO
